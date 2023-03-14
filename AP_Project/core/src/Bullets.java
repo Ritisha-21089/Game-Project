a5886ac81a909e6c5f878c5be9d18fc2a309e86e
Shriya Verma <104719408+waningcrescent@users.noreply.github.com>
2023-03-14 16:59:38 +0530

    Update Bullets.java

--- AP_Project/core/src/Bullets.java
+++ AP_Project/core/src/Bullets.java
@@ -7,21 +7,69 @@ import java.util.ArrayList;
 
 public class Bullets extends Lab1 implements Cloneable, InputProcessor {
 
-    public static ArrayList<Sprite> bul=new ArrayList<>();
-    public Bullets() {
-        Texture bombT = new Texture("circle.png");
-        Sprite bomb = new Sprite(bombT);
-        bomb.setSize(50, 50);
-        bomb.setPosition(0,0);
-        bul.add(bomb);
+    private Integer damage;
+    private Double radius;
+    private Integer mass;
+    private Body bulbody;
+    private Sprite bulSprite;
+    public Bullets(Integer positionX, Integer positionY, World world) {
+        bulSprite = new Sprite(new Texture("circle.png"));
+        bulSprite.setSize(34, 34);
+        createbulbody(positionX, positionY, world);
+        
     }
-    public float moveBullet(int u, int theta){
+    
+    public Bullets() {}
+    
+    public Bullets (int damage, double radius, int mass) {
+        this.damage = damage;
+        this.radius = radius;
+        this.mass = mass;
+    }
+    
+   /* public float moveBullet(int u, int theta){
         Gdx.input.setInputProcessor(this);
         float range = (float) (u*u*Math.sin(2*theta*(Math.PI)/180)/9.81);
         return range;
-        }
+        } */
 
-    @Override
+     public void createbulBody(Integer positionX, Integer positionY, World world) {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(positionX, positionY);
+        bulBody = world.createBody(bodyDef);
+
+        CircleShape circleShape = new CircleShape();
+        circleShape.setRadius(5);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = circleShape;
+        fixtureDef.density = 1f;
+
+        bulBody.createFixture(fixtureDef).setUserData("bomb");
+        circleShape.dispose();
+    }
+
+     public double convertToRadians(float angle) {
+        return Math.PI * angle / 180.0;
+    }
+
+     public void fire(float power, float angle) {
+        angle = (float) convertToRadians(angle);
+        float x = power * (float) Math.cos(angle);
+        float y = power * (float) Math.sin(angle);
+        Vector2 impulse = new Vector2(x, y);
+        bulBody.applyLinearImpulse(impulse, bulBody.getWorldCenter(), true);
+    }
+    
+     public void updateImage(SpriteBatch batch) {
+        int x = (int) bulBody.getWorldCenter().x;
+        int y = (int) bulBody.getWorldCenter().y;
+        bulSprite.setPosition(x - 16 , y - 16);
+        bulSprite.draw(batch);
+    }
+    
+   @Override
     public boolean keyDown(int keycode) {
         return false;
     }
