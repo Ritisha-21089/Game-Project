674767846536e0e410b91f999c15de63e5613952
Shriya Verma <104719408+waningcrescent@users.noreply.github.com>
2023-03-14 16:19:23 +0530

    Update savedgame.java

--- AP_Project/core/src/savedgame.java
+++ AP_Project/core/src/savedgame.java
@@ -6,30 +6,30 @@ public class SavedGame implements Serializable {
     // save the game state using json serialization and deserialization
     // save player position, health, fuel
     private static final long serialVersionUID = 1L;
-    private Player player1;
-    private Player player2;
+    private Player p1;
+    private Player p2;
     private int turn;
 
-    public SavedGame(Player player1, Player player2, int turn) {
-        this.player1 = player1;
-        this.player2 = player2;
+    public SavedGame(Player p1,Player p2, int turn) {
+        this.player1 = p1;
+        this.player2 = p2;
         this.turn = turn;
     }
 
     public Player getPlayer1() {
-        return player1;
+        return p1;
     }
 
     public void setPlayer1(Player player1) {
-        this.player1 = player1;
+        this.p1 = player1;
     }
 
     public Player getPlayer2() {
-        return player2;
+        return p2;
     }
 
     public void setPlayer2(Player player2) {
-        this.player2 = player2;
+        this.p2 = player2;
     }
 
     public int getTurn(){
